b824c81329aacba24a64ac2d3d932f3c448b28d3
mahjujah usrat <mahjujahusratvina@gmail.com>
2021-11-06 00:35:26 +0600

    finished

---
+++ src/Pages/Firebase/Firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAZKaLbuIpLM4rhIAS9onc9niSXocKwGXQ",
+  authDomain: "travel-turizam.firebaseapp.com",
+  projectId: "travel-turizam",
+  storageBucket: "travel-turizam.appspot.com",
+  messagingSenderId: "802355078966",
+  appId: "1:802355078966:web:c56e3cca0a092cfb49f597"
+};
+
+export default firebaseConfig;
